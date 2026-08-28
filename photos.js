0ce955a3405320d2b947983e094c072ede4961f2
wearelovingawareness-oss <wearelovingawareness@gmail.com>
2026-08-29 10:03:47 +1200

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,347 @@
+// H.E.A.R.T.S. photo library — generated from gallery.html. Original photos are untouched.
+window.HEARTS_PHOTOS = [
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586305834269-H9JM5WHKXG5EXUPZZ0T3/2108+Boy+%26+Olli.jpg",
+  "name": "2108 Boy & Olli"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554259525272-SG0N5PPL0S9PCWC1F9XL/Petro+%26+Indy.jpg",
+  "name": "Petro & Indy"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586305052814-MU6R3PFIBE4U5DP8ZJKS/Jan+2012+from+2011+1327.JPG",
+  "name": "Jan 2012 from 2011 1327"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586319809373-BC42ZJK36PEQE7VHXTZM/Cropped+one.jpg",
+  "name": "Cropped one"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554259013517-RX6XMLAOOGEVJNCYCGDG/20180221_163015.jpg",
+  "name": "Photo 5"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1558482781070-7ZNVI6HGSVAKJ3GVB3XP/IMG_20160417_151254591_HDR.jpg",
+  "name": "IMG 20160417 151254591 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1543792819244-X0D5BJV0IV8DBDA2R89L/Horse+Sense+Aug+2014-135.jpg",
+  "name": "Horse Sense Aug 2014-135"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1543793790502-34NQT9EG1IQDFT0T108Y/IMG_20160418_150750048_HDR.jpg",
+  "name": "IMG 20160418 150750048 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586305366730-NWEFOZW31ZE482LRLY3R/20181202_104917.jpg",
+  "name": "Photo 9"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586305681531-8X4OZYNNPE5VVME6IJI6/20180225_112212.jpg",
+  "name": "Photo 10"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553663746452-S5XTXDVRK7GEX67NHNEN/12.jpg",
+  "name": "12"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553663497362-2NC0A67Z35DFBZJH6EDD/4.jpg",
+  "name": "4"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553663663671-Y0OLPBOH5RSKQ3SQIJBG/10.jpg",
+  "name": "10"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662783314-UBTKOK992JC7JHROFZRN/3.jpg",
+  "name": "3"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662799043-YWAKSB4E9HMYVSZBLF8C/5.jpg",
+  "name": "5"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662804263-8KQ294KKTM45O4PFVSNU/6.jpg",
+  "name": "6"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662895571-CN2AVN4RTQHNYCK95JBU/11.jpg",
+  "name": "11"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662923414-YDMZENKHZ0TKOHJ3W6KA/14.jpg",
+  "name": "14"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662926389-UYJUGUMGHGOULVG219M5/13.jpg",
+  "name": "13"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662865367-RPJHWAIUOYMXWJ5PKCSP/9.jpg",
+  "name": "9"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553663600205-8F9FOCDCUFNBECRM27P5/1.jpg",
+  "name": "1"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662944276-BIUWU7486XHGX722O7QS/15.jpg",
+  "name": "15"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553662943629-75P5M5HVGEF410JWNPUV/16.jpg",
+  "name": "16"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554260927341-CZ8KHW5JK7V0XUEI54LX/8.jpg",
+  "name": "8"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553494140849-DIHKP5R6PNDJTV5F6IBY/16806888_10154477974005735_5387071460139531644_n.jpg",
+  "name": "Photo 25"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553494166871-V97GPCL7XUO2KJY0YLJ4/16864698_10154408736526847_8555454789932498751_n.jpg",
+  "name": "Photo 26"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493905439-QO2GICC9VG1TFRJJS9R5/16938573_10206778724341713_8409273002471465405_n.jpg",
+  "name": "Photo 27"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553494224451-4HE60XKFHSH72DTIUS6X/16938600_10206778712181409_1125584835306108566_n.jpg",
+  "name": "Photo 28"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493908229-ESYIUZQ2JAJ0N1CC6WBM/16939351_10154408740201847_2717182322625603988_n.jpg",
+  "name": "Photo 29"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493908255-HRQ4J15K4FBQ4Q7Y3WYE/16998220_10154477966340735_216332141421047323_n.jpg",
+  "name": "Photo 30"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493910411-ZUWMFNJZLZDDDSEV71M0/16998844_10154408746001847_716181489262960139_n.jpg",
+  "name": "Photo 31"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553494271460-R1D4A3MQRZS9KO7CXZVP/17021762_10206778716661521_1826526566307058425_n.jpg",
+  "name": "Photo 32"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493912565-3LY8ZQ0F7M7VKUBAW0Y5/17021826_10206778757142533_3779426769242271233_n.jpg",
+  "name": "Photo 33"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493912885-ZT8437GJOY76Q1E4VR2O/17021863_10154408744221847_8100910169562769562_n.jpg",
+  "name": "Photo 34"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553493914792-OSXEYQ19XU7U3H27CL58/17103751_10206778748302312_1870617626127976130_n.jpg",
+  "name": "Photo 35"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302565883-Q2O0EI0JLEPK76GPI93A/91982889_2650074098593896_3838514492656844800_n.jpg",
+  "name": "Photo 36"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302568028-6PMY7KUV4C44XHZ5RU7N/92096996_257950022271889_280357675328339968_n.jpg",
+  "name": "Photo 37"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302569149-J4C1A4XZCOROSY6UK758/92175969_223661145711538_6070935989428158464_n.jpg",
+  "name": "Photo 38"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302570849-RMM3B1KGSFEZ5RJ76QV1/92245135_219141739338427_7404846436635901952_n.jpg",
+  "name": "Photo 39"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302572529-86W4YRF0DTA25KYIKK5U/92344718_538769126657463_6484489023036325888_n.jpg",
+  "name": "Photo 40"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302573753-M6IYOXLJ9WAN6VVD30WH/92444117_257258568743135_6749253888316014592_n.jpg",
+  "name": "Photo 41"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302574147-655K385U4YZIE9WNQLSK/92459021_218575146135623_4556782377740795904_n.jpg",
+  "name": "Photo 42"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1586302574812-AWK2QRKMONNXDFKC8L7U/92479502_644665252768329_3608535254702227456_n.jpg",
+  "name": "Photo 43"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553497038845-3U27TX1QTYI97U6Y1F6A/2018+Horses+lying+down.jpg",
+  "name": "2018 Horses lying down"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554678012463-0RP9W3CGBXB3C00PWZ83/IMG_20171017_124832.jpg",
+  "name": "IMG 20171017 124832"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554678006719-GRWCD8ERNI8FFZI45KXN/IMG_20171017_150059.jpg",
+  "name": "IMG 20171017 150059"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496440014-VA4V4T8NG66QG9PANR3R/IMG_20160418_125052528_HDR.jpg",
+  "name": "IMG 20160418 125052528 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554262448552-SJNB7U7QYMZ4X8F2IYS4/IMG_20160417_151451936_HDR.jpg",
+  "name": "IMG 20160417 151451936 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496384052-B7BZSZSNVB60DZTNWVGJ/IMG_20160417_121005676_HDR.jpg",
+  "name": "IMG 20160417 121005676 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496365455-LJ4FZDAMBH5IK088ZNRU/IMG_20160417_094800579.jpg",
+  "name": "IMG 20160417 094800579"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496472360-2NZUT4S15B2VSTD4JR6I/IMG_20160418_130306320.jpg",
+  "name": "IMG 20160418 130306320"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496479516-U7N4JNJWWCDK63DDXT92/IMG_20160418_150750048_HDR.jpg",
+  "name": "IMG 20160418 150750048 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496497290-XNJORD27G9O6AMTLFV92/IMG_20160418_152139557_HDR.jpg",
+  "name": "IMG 20160418 152139557 HDR"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496952789-DFF4SDEWM2PUK9FZAY34/14713614_10154052310591847_8138602310949970260_n.jpg",
+  "name": "Photo 54"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496941185-6XV0CZ9ZD6ZTOJWZGKGR/11220802_10154052359521847_5376210265519006829_n.jpg",
+  "name": "Photo 55"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496940850-C7IBKA0UJ174ZO5SHG3E/14639578_10154052357486847_7570416887031464555_n.jpg",
+  "name": "Photo 56"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496945021-NR4GLJJS2PVLWOEYOLNJ/14657306_10154052359806847_9054419659198928023_n.jpg",
+  "name": "Photo 57"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496945608-13Y9WJM94K8DU6OD2V48/14666059_10154052346451847_180043257069797718_n.jpg",
+  "name": "Photo 58"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496949340-5RTRRYDUY5YRPPAM7QV1/14681752_10154052362141847_4624057804912264823_n.jpg",
+  "name": "Photo 59"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496949341-5JYURJH84U0WICU4Y5N1/14702346_10154052350196847_7944947139551829546_n.jpg",
+  "name": "Photo 60"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496952751-5BX5H81B1QBHZ7DXO182/14708107_10154052360306847_6861973845465400147_n.jpg",
+  "name": "Photo 61"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496955939-NATLZXVYJNZRVN0LUL14/14720379_10154052362691847_8036169605727448832_n.jpg",
+  "name": "Photo 62"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553496957347-D8IFKV3LL9GVKX44UDPH/14729403_10154052359556847_6419939497102010064_n.jpg",
+  "name": "Photo 63"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554261529406-FUO8NT52OTYP1UF6R04I/Oli%27s+head.jpg",
+  "name": "Oli's head"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1548980188356-U1B22AHWTEMFDDF6I5KJ/Indi+lying+down+2018.jpg",
+  "name": "Indi lying down 2018"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1548978579022-2Y8164ADKQE3YAX14BMW/20161219_125738.jpg",
+  "name": "Photo 66"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1548978971648-57MPWNWRK3RA6911L0CR/Venus+2.jpg",
+  "name": "Venus 2"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554261389831-ZBGSKN35PGAXQOG3SYIQ/Monique.jpg",
+  "name": "Monique"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1553629341772-2JCDYR9A53K8ACAO23MT/Tom+lying+down+and+Lisa.jpg",
+  "name": "Tom lying down and Lisa"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554262065160-975XODPUWEIB40O0KNYI/Olly+and+boy.jpg",
+  "name": "Olly and boy"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1554262188846-0VQRVZAIO07FCC6I7RTM/Juju.jpg",
+  "name": "Juju"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/5bee6347e74940c7452998c5/1594872776003-EFVQR6QGBTMD9Z7LABMH/60921608_2502539029777313_4697896710821642240_o.jpg",
+  "name": "Photo 72"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1778716355451-DIM41V1GYVOM31ZYS11J/Renee%2Band%2BTom%2Bcropped%2Bone.jpg",
+  "name": "Renee and Tom cropped one"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/6108d69f-be70-45ef-afd1-462352dec2f9/Abby+Indi.jpg",
+  "name": "Abby Indi"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1725598796040-51VC2VH416UB7TWRJXWW/Hongi.jpg",
+  "name": "Hongi"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1709901301387-136FH6TFO78INW4RRDA4/Boys+and+Frank.JPG",
+  "name": "Boys and Frank"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1658537633222-G5KL4TRJW7ZAJ76O46ED/Lisa+%26+Frank.jpg",
+  "name": "Lisa & Frank"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1696985813441-JI5NNMI908Q8WU9QDTIG/Group+1.jpg",
+  "name": "Group 1"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1778716441063-3OW5KB3PK8JV4RBAPXQ8/20200519_134040.jpg",
+  "name": "Photo 79"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1732844877240-TCEJ8CV67LYK0FBWS6V4/Abi+and+Olly.jpg",
+  "name": "Abi and Olly"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1710635488960-WQZCOVKKVHZ3LW148C4U/Best+of+Tubby+%26+Keegan.JPG",
+  "name": "Best of Tubby & Keegan"
+ },
+ {
+  "src": "https://images.squarespace-cdn.com/content/v1/623820c2d1350755a59b6d22/1657613563028-GS2ACFU9XJ65NOGF27AH/2108+Boy+%26+Olli.jpg",
+  "name": "2108 Boy & Olli"
+ }
+];
+
+// Jenny's target slideshow order (descriptions only — photos assigned in the Slideshow Manager)
+window.HEARTS_SLOTS = [
+ "Boy and Ollie",
+ "Man — Stand Strong and Ollie",
+ "Group of adults sitting down",
+ "Petro and Indi",
+ "Group standing — no horse",
+ "Abby, Marisol and Indi",
+ "Reuben hugging Tom",
+ "Youth group — no horse",
+ "Lisa and Frank",
+ "Hi-vis man and Ollie",
+ "Girl in pink and horse",
+ "Renee and Tom — sitting down"
+];
